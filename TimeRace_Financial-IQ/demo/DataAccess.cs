1fc498192cb121dd05b9034ff3053770ad36af87
冰冰 <34570791+lbbgit@users.noreply.github.com>
2018-01-04 16:10:56 +0800

    Add some class files ...

---
+++ TimeRace_Financial-IQ/demo/DataAccess.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Reflection;
+
+using System.Data;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+public interface IBase{}
+public sealed class DataAccess<T> where T : IBase
+{
+    private static readonly string dcPath = "";
+    private static readonly string nameSpace = "";
+
+
+    private static object lockHelper = new object();
+    private static T obj = default(T);
+
+    public static T CreateInstance( )  //Config.Databasetype type
+    {
+        string className = nameSpace + "." + typeof(T).Name.Substring(1);
+        if (obj == null)
+        {
+            //make sure that 'Static Instace' is only
+            lock (lockHelper)
+            {
+                if (obj == null)
+                {
+                    obj = (T)Assembly.Load(dcPath).CreateInstance(className);
+                }
+            }
+        }
+        return obj;
+    }
+     
+    /// <summary>
+    /// String.Split()
+    /// </summary> 
+    public List<string> StringSplit(string str,char split_char=',')
+    {
+        List<string> devs = new List<string>();
+
+        int start = 0;
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (str[i] == split_char)
+            {
+                devs.Add(str.Substring(start, i));
+                start = i + 1;
+            }
+        }
+
+        return devs;
+    }
+         
+}
---
+++ TimeRace_Financial-IQ/demo/MemoryHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Dynamic;
+using System.Runtime.InteropServices;//DllImport
+
+using System.Threading;
+using System.Diagnostics;//Process
+public class MemoryHelper
+{
+    //在程序中用一个计时器，每隔几秒钟调用一次该函数，打开任务管理器，你会有惊奇的发现
+
+    #region 内存回收
+    [DllImport("kernel32.dll", EntryPoint = "SetProcessWorkingSetSize")]
+    public static extern int SetProcessWorkingSetSize(IntPtr process, int minSize, int maxSize);
+    /// <summary>
+    /// 释放内存
+    /// </summary>
+    public static void ClearMemory_old()
+    {
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+        {
+            //App 
+            //App.SetProcessWorkingSetSize(System.Diagnostics.Process.GetCurrentProcess().Handle, -1, -1);
+        }
+    }
+
+
+
+
+    //网上大多推荐使用系统的SetProcessWorkingSetSize的函数API，但是经过实践发现并不好用。建议使用EmptyWorkingSet函数。
+    [DllImport("psapi.dll")]
+    static extern int EmptyWorkingSet(IntPtr hwProc);
+
+    /// <summary>
+    /// 释放内存
+    /// </summary>
+    public static void ClearMemory()
+    {
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        Process[] processes = Process.GetProcesses();
+        foreach (Process process in processes)
+        {
+            //对于系统进程会拒绝访问，导致出错，此处对异常不进行处理。
+            try
+            {
+                EmptyWorkingSet(process.Handle);
+            }
+            catch { }
+        }
+    }
+    #endregion
+}
